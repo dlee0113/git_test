302afe333962ded4e3aa66ee23f8937cf5fa88d4
dlee0113 <dlee0113@gmail.com>
2013-10-02 07:31:14 -0400

    new feature added to a branch

--- eclipse_egit_test/src/Test.java
+++ eclipse_egit_test/src/Test.java
@@ -2,5 +2,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("pull this for rebase");
 		System.out.println("push this after rebase");
+		System.out.println("new feature added to a branch");
 	}
 }
